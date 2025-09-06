08bd223823f31bf15749728276bc17f16a779243
Praveen T <praveentpraveen01@gmail.com>
2025-09-06 13:31:06 +0530

    Add files via upload

---
+++ CyclicSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = { 3, 4, 5, 2, 1 };
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cycle(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
