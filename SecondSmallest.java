969a53d32b913b292dce8dc67e06a327504f06bb
Praveen T <praveentpraveen01@gmail.com>
2025-09-10 19:03:12 +0530

    Add files via upload

---
+++ SecondSmallest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SecondSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[1]);
+    }
+}
