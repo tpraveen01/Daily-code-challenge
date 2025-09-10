bf0b0c34f654d528fe77f76b125db871c4d2832e
Praveen T <praveentpraveen01@gmail.com>
2025-09-10 19:27:31 +0530

    Add files via upload

---
+++ SumArr.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SumArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
