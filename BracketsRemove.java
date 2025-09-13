f84fac95cd3f93d2f04e1e503bcc418d084691f7
Praveen T <praveentpraveen01@gmail.com>
2025-09-13 23:20:23 +0530

    Create BracketsRemove.java

---
+++ BracketsRemove.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class BracketsRemove{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='(' || ch==')'){
+                ch++;
+            }else{
+                System.out.print(ch);
+            }
+        }
+    }
+}
