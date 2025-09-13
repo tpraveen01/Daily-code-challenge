97ca6d03134381fc52e9a7cdef96da9c3ee366b1
Praveen T <praveentpraveen01@gmail.com>
2025-09-13 23:03:44 +0530

    Create RemoveSpace.java

---
+++ RemoveSpace.java
@@ -0,0 +1,14 @@
+public class RemoveSpace{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch==' '){
+                ch++;
+            }else{
+                System.out.print(ch);
+            }
+        }
+    }
+}
