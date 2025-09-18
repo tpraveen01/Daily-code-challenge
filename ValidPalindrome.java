38a219b5f04fa5a6b170db35f264c083c30e17e3
Praveen T <praveentpraveen01@gmail.com>
2025-09-18 21:33:03 +0530

    Create ValidPalindrome.java

---
+++ ValidPalindrome.java
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int start=0;
+        int end=s.length()-1;
+        while(start<=end){
+            if(s.charAt(start) !=s.charAt(end)){
+                return false;
+            }else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
