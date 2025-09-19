21b8870cdd78b96250d1113f048b9b058511318c
Praveen T <praveentpraveen01@gmail.com>
2025-09-19 18:22:57 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/valid-anagram/?envType=problem-list-v2&envId=string
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+        if(m!=n){
+            return false;
+        }
+        int [] count=new int[26];
+        for(int i=0;i<m;i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<count.length;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
