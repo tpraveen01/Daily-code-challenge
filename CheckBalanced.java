992609e003a6cdc61679faf99c041709224ecdd2
Praveen T <praveentpraveen01@gmail.com>
2025-09-19 19:35:37 +0530

    Create CheckBalanced.java

---
+++ CheckBalanced.java
@@ -0,0 +1,19 @@
+3340.//leetcode.com/problems/check-balanced-string/?envType=problem-list-v2&envId=string
+class Solution {
+    public boolean isBalanced(String num) {
+        int evensum=0;
+        int oddsum=0;
+        for(int i=0;i<num.length();i++){
+            if(i%2==0){
+                evensum+=num.charAt(i)-'0';
+            }else{
+                oddsum+=num.charAt(i)-'0';
+            }
+            if(evensum==oddsum){
+                return true;
+            }
+        }
+        // return evensum==oddsum;
+        return false;
+    }
+}
